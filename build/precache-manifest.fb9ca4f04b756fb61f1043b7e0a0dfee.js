53431427e3ebeef04aa596e65d1c98420fe4d23a
VedantWankhade <vedantwankhade17@gmail.com>
2021-08-05 02:55:54 +0530

    connect to firebase

---
+++ build/precache-manifest.fb9ca4f04b756fb61f1043b7e0a0dfee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b5619d23501be3856e7225edc1cccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93979e8565bddad8be70",
+    "url": "/static/css/main.a8f0c9e1.chunk.css"
+  },
+  {
+    "revision": "04a393347f75da8b1797",
+    "url": "/static/js/2.b7bd8724.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.b7bd8724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93979e8565bddad8be70",
+    "url": "/static/js/main.0acbec86.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,22 @@ import firebase from 'firebase/app';
 import "firebase/auth";
 
 export const auth =  firebase.initializeApp({
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyBaW4qmE1vHlq-_3DQlHbZyknm6jWazJh0",
+    authDomain: "chatify-59f3f.firebaseapp.com",
+    projectId: "chatify-59f3f",
+    storageBucket: "chatify-59f3f.appspot.com",
+    messagingSenderId: "293020537066",
+    appId: "1:293020537066:web:4be1429f98b209aaa94793",
+       measurementId: "G-BMS64GRF1F"
   }).auth();
+
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// export const auth = {
+//   apiKey: "AIzaSyBaW4qmE1vHlq-_3DQlHbZyknm6jWazJh0",
+//   authDomain: "chatify-59f3f.firebaseapp.com",
+//   projectId: "chatify-59f3f",
+//   storageBucket: "chatify-59f3f.appspot.com",
+//   messagingSenderId: "293020537066",
+//   appId: "1:293020537066:web:4be1429f98b209aaa94793",
+//   measurementId: "G-BMS64GRF1F"
+// };
