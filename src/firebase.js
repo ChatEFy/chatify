57c76b740fe55d6c3cf22e181616631aec204aa2
VedantWankhade <vedantwankhade17@gmail.com>
2021-08-05 03:15:24 +0530

    Store firebase and other sensitive data into env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,20 @@
 import firebase from 'firebase/app';
 import "firebase/auth";
 
-export const auth =  firebase.initializeApp({
-    apiKey: "AIzaSyBaW4qmE1vHlq-_3DQlHbZyknm6jWazJh0",
-    authDomain: "chatify-59f3f.firebaseapp.com",
-    projectId: "chatify-59f3f",
-    storageBucket: "chatify-59f3f.appspot.com",
-    messagingSenderId: "293020537066",
-    appId: "1:293020537066:web:4be1429f98b209aaa94793",
-       measurementId: "G-BMS64GRF1F"
-  }).auth();
+const apiKey = process.env.API_KEY;
+const authDomain = process.env.AUTH_DOMAIN;
+const projectId = process.env.PROCESS_ID;
+const storageBucket = process.env.STORAGE_BUCKET;
+const messagingSenderId = process.env.MESSAGING_SENDER_ID;
+const appId = process.env.APP_ID;
+const measurementId = process.env.MEASSUREMENT_ID;
 
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// export const auth = {
-//   apiKey: "AIzaSyBaW4qmE1vHlq-_3DQlHbZyknm6jWazJh0",
-//   authDomain: "chatify-59f3f.firebaseapp.com",
-//   projectId: "chatify-59f3f",
-//   storageBucket: "chatify-59f3f.appspot.com",
-//   messagingSenderId: "293020537066",
-//   appId: "1:293020537066:web:4be1429f98b209aaa94793",
-//   measurementId: "G-BMS64GRF1F"
-// };
+export const auth =  firebase.initializeApp({
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+    measurementId
+  }).auth();
